2a9c812bb55a4feb325108587dc0e0992b878110
Azhar Ibn Mostafiz <theazharul@gmail.com>
2020-04-15 19:59:20 +0600

    some change

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/org/Habits.org")))
  '(package-selected-packages
    (quote
     (ob-elixir org-bullets zenburn-theme auto-complete company-mode magit yasnippet-snippets yasnippet web-mode alchemist evil)))
