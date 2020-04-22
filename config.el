5c9348ea2749d3a2d199ef966f2ceace8c0ea456
Azhar Ibn Mostafiz <theazharul@gmail.com>
2020-04-22 20:15:53 +0600

    My Spacemacs Configuration

---
+++ config.el
@@ -0,0 +1,19 @@
+(setq dotspacemacs-editing-style 'vim)
+(defun dotspacemacs/user-config ()
+    "Configuration function for user code.
+This function is called at the very end of Spacemacs initialization after
+layers configuration.
+This is the place where most of your configurations should be done. Unless it is
+explicitly specified that a variable should be set before a package is loaded,
+you should place your code here."
+
+(add-hook 'dart-mode-hook 'lsp)
+
+(use-package flutter
+  :after dart-mode
+  :bind (:map dart-mode-map
+              ("C-M-x" . #'flutter-run-or-hot-reload))
+  :custom
+  (flutter-sdk-path "/opt/flutter/"))
+    )
+
